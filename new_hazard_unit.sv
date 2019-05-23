dfb0248302eb2a5d76156d0c46680f90ad4e1a32
Saratvk <saratavakoli77@ut.ac.ir>
2019-05-23 16:37:13 +0430

    New_hazard_unit updated

---
+++ new_hazard_unit.sv
@@ -0,0 +1,15 @@
+module new_hazard_unit( input jump, branch_equal, branch_not_equal, equal, not_equal, output reg flush, pc_ld, IF_ID_write);
+	always@(*) begin
+		flush = 0;
+		IF_ID_write = 1;
+		pc_ld = 1;
+		//flush after jump
+		if(jump)
+			flush = 1;
+
+		//flush after beq or bneq
+		if( (branch_equal && equal) || (branch_not_equal && not_equal) )
+			flush = 1;
+
+	end
+endmodule // new_hazard_unit
