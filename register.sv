28dde64736945ba57aa7bc89dade6503895a1832
rasta <rasta.tadayon1378@gmail.com>
2019-05-23 16:50:39 +0430

    register

---
+++ register.sv
@@ -0,0 +1,8 @@
+module register #(parameter n = 32)(input clk, rst, ld, input[n-1 : 0] in, output reg[n-1 : 0] out);
+	always@(posedge clk, posedge rst) begin
+		if(rst)
+			out <= {n{1'b0}};
+		else
+			out <= in;
+	end
+endmodule
