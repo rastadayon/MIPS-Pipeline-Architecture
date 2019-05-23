3951e5592dd566bc6ecf89f564b73e72b9a2248c
Saratvk <saratavakoli77@ut.ac.ir>
2019-05-23 19:18:52 +0430

    MEM_WB updated

--- MEM_WB.sv
+++ MEM_WB.sv
@@ -1,14 +1,16 @@
 `timescale 1ns/1ns
-module MEM_WB (input clk, rst, input [1:0] MEM_WB_wb, input [31:0] MEM_WB_read_data, MEM_WB_alu_res, input [4:0] MEM_WB_reg_dest, output reg [1:0] MEM_WB_wb_out, output reg [31:0] MEM_WB_read_data_out, MEM_WB_alu_res_out, output reg [4:0] MEM_WB_reg_dest_out);
+module MEM_WB (input clk, rst, input MEM_WB_mem_to_reg, MEM_WB_reg_write, input [31:0] MEM_WB_read_data, MEM_WB_alu_res, input [4:0] MEM_WB_reg_dest, output reg MEM_WB_mem_to_reg_out, MEM_WB_reg_write_out, output reg [31:0] MEM_WB_read_data_out, MEM_WB_alu_res_out, output reg [4:0] MEM_WB_reg_dest_out);
 	always@(posedge clk, posedge rst) begin
 		if (rst) begin
-			MEM_WB_wb_out <= 2'b0;
+			MEM_WB_mem_to_reg_out <= 1'b0;
+			MEM_WB_reg_write_out <= 1'b0;
 			MEM_WB_read_data_out <= 32'b0;
 			MEM_WB_alu_res_out <= 32'b0;
 			MEM_WB_reg_dest_out <= 5'b0;
 		end // if (rst)
 		else begin
-			MEM_WB_wb_out <= MEM_WB_wb;
+			MEM_WB_mem_to_reg_out <= MEM_WB_mem_to_reg;
+			MEM_WB_reg_write_out <= MEM_WB_reg_write;
 			MEM_WB_read_data_out <= MEM_WB_read_data;
 			MEM_WB_alu_res_out <= MEM_WB_alu_res;
 			MEM_WB_reg_dest_out <= MEM_WB_reg_dest;
