4749d0659405d16a98b423f5d458933877906f0f
rasta <rasta.tadayon1378@gmail.com>
2019-05-27 17:30:07 +0430

    ..

--- comparator.sv
+++ comparator.sv
@@ -1,6 +1,12 @@
-module comparator #(parameter n)(input [n-1 : 0] in0, in1, output equal, notEqual);
-	assign equal = (in0 == in1) ? 1'b1 :
+module comparator #(parameter n)(input [n-1 : 0] in0, in1, output reg equal, notEqual);
+	/*assign equal = (in0 == in1) ? 1'b1 :
 	1'b0;
 	assign notEqual = (in0 != in1) ? 1'b1 :
-	1'b0;
+	1'b0;*/
+	always@(in0,in1) begin
+		if (in0 == in1)
+			equal <= 1'b1;
+		else
+			notEqual <= 1'b1;
+	end
 endmodule
